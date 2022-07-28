2cd0f55468ac058fc4ead49a85ac78aa1d799119
ha-manel <manelhammouche3@gmail.com>
2022-07-28 08:17:52 +0100

    Add solution

--- Challenge.js
+++ Challenge.js
@@ -1,1 +1,11 @@
-
+function digital_root(n) {
+  while (n.toString().length > 1) {
+    let sum = 0;
+    while (n) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    }
+    n = sum;
+  }
+  return n;
+}
